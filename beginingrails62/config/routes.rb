377120235098b4a096883f4bb69f5b8dd086a77f
AlexUkPC <alexrogna@yahoo.com>
2021-04-01 12:04:40 +0100

    changed to WSL

---
+++ beginingrails62/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: "articles#index"
+  resources :articles do
+    resources :comments
+  end
+  resources :users
+  resource :session
+  get "/login", to: "sessions#new", as: "login"
+  get "/logout", to: "sessions#destroy", as: "logout"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
